87fc55d0250bad440f22b6e625fcdb739206fb24
Timothy DeBerry <timothy.c.deberry2.mil@army.mil>
2022-12-01 15:27:08 +0000

    Rename src files.

---
+++ 25/src/main.c
@@ -0,0 +1,14 @@
+/** @file main.c
+* 
+* @brief A description of the module's purpose. *****CHANGE ME*****
+*
+*/
+
+#include <stdio.h>
+
+#include "../include/Advent_Of_Code_2022.h"
+
+int main()
+{
+
+}
