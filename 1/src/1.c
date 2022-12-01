24ae69ae98f6481d135c85bfbc2589bbad2dd426
Timothy DeBerry <timothy.c.deberry2.mil@army.mil>
2022-12-01 15:56:40 +0000

    Create function for solving AOC day 1 problem.

--- 1/src/1.c
+++ 1/src/1.c
@@ -8,4 +8,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../include/Advent_Of_Code_2022.h"
+#include "../include/1.h"
+
+void calculate_calories(FILE * file_ptr)
+{
+	char line[256];
+        // char prev_line[256];
+        int high = 0;
+        int number = 0;
+        // int length = 0;
+    
+        // read in contents of file
+        while (fgets(line, 256, file_ptr) != NULL)
+        {
+            if (strcspn(line, "\n") != 0)
+            {
+
+                line[strcspn(line, "\n")] = 0;
+                number += atoi(line);
+                // printf("%s\n", line);
+                // number = 0;
+                // // printf("%s\n", line);
+                // number += atoi(line);
+                
+                
+            }
+            else
+            {
+                // printf("%d\n", number);
+                if (number > high)
+                {
+                    high = number;
+                    
+                    // number = 0;
+                }
+                number = 0;
+
+            }
+
+        }
+
+        printf("%d\n",high);
+}
